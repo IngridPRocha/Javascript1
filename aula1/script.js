2cd6c7c0db78ffd19006bf5ad19216690bf3205a
IngridPRocha <rochaingrid63@gmail.com>
2023-07-04 18:36:25 -0300

    recursos nativos

---
+++ aula1/script.js
@@ -0,0 +1,17 @@
+//comentario em javascript de uma linha
+    /*comentario mais de uma linha*/
+
+//RECURSOS NATIVOS
+
+//Console 
+
+console.log('Boa noite Pessoal')
+console.info('console de inormaçao.')
+console.warn('cuidado')
+console.error('aqui temos uma mensagem de erro')
+
+//caixa de dialogo
+
+window.alert('janela de alerta')//janela de alerta
+window.confirm('tudo bem pessoal?')//janela de confirmaçao
+window.prompt('qual seu nome?')//janela para obter algum dado do usuario
